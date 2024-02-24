a67bd212bf33d2f1b7b79e71bbf015729ad47839
Juan Espinola <juan.espinola@itti.digital>
2024-02-24 13:00:40 +0000

    nuevo formato

--- app/Http/Controllers/FederationController.php
+++ app/Http/Controllers/FederationController.php
@@ -10,27 +10,50 @@ class FederationController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $headers = [
-            (object)[ 
-                'title' => 'Id',
-                'key' => 'id', 
-                'active' => true, 
-                'order' => 'asc', 
-                'visible' => true,
-                'ignore' => false
+        // $headers = [
+        //     (object)[ 
+        //         'title' => 'Id',
+        //         'key' => 'id', 
+        //         'active' => true, 
+        //         'order' => 'asc', 
+        //         'visible' => true,
+        //         'ignore' => false
             
-            ],
-            (object)[ 'title' => 'Descripcion','key' => 'description' ]
-        ];
-        $columns = [
-            (object)[ 'title' => 'Id','key' => 'id', 'active' => true, 'order' => 'asc', 'visible' => true ],
-            (object)[ 'title' => 'Descripcion','key' => 'description', 'active' => true, 'order' => 'asc', 'visible' => true ]
-        ];
-        $entity = "federation";
-        $order = true;
-        return view('tabla', compact(['headers', 'columns', 'entity', 'order']));
+        //     ],
+        //     (object)[ 'title' => 'Descripcion','key' => 'description' ]
+        // ];
+        // $columns = [
+        //     (object)[ 'title' => 'Id','key' => 'id', 'active' => true, 'order' => 'asc', 'visible' => true ],
+        //     (object)[ 'title' => 'Descripcion','key' => 'description', 'active' => true, 'order' => 'asc', 'visible' => true ]
+        // ];
+        // $entity = "federation";
+        // $order = true;
+        // return view('tabla', compact(['headers', 'columns', 'entity', 'order']));
+
+        try {
+            // Obtén los parámetros de la solicitud
+            $limit = $request->input('limit', 5); // Valor predeterminado de 10 si no se proporciona
+            $page = $request->input('page', 1);   // Página predeterminada de 1 si no se proporciona
+            $sortBy = $request->input('sortBy', 'id'); // Campo de orden predeterminado si no se proporciona
+            $order = $request->input('order', 'asc'); // Dirección de orden predeterminada si no se proporciona
+
+            // Construye la consulta
+            $query = Federation::query();
+            // Aplica orden
+            $query->orderBy($sortBy, $order);
+
+            // Aplica límite
+            $query->take($limit);
+
+            // Realiza la paginación
+            $data = $query->paginate($limit, ['*'], 'page', $page);
+            return response()->json($data, 200);
+        } catch (\Throwable $th) {
+            throw $th;
+            return response()->json($th, 400);
+        }
     }
 
     /**
